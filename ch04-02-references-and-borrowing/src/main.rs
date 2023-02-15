59c420e2f58f1cb8959e8c78ddbac9324fd1f043
metfairy <metfairy@gmail.com>
2023-02-15 12:47:02 +0000

    ch04-02-references-and-borrowing

---
+++ ch04-02-references-and-borrowing/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // 没问题
+    let r2 = &s; // 没问题
+    println!("{} and {}", r1, r2);
+    // 此位置之后 r1 和 r2 不再使用
+
+    let r3 = &mut s; // 没问题
+    println!("{}", r3);
+
+    let mut s = "word";
+
+    let r1 = s; // 没问题
+    let r2 = s; // 没问题
+    println!("{} and {}", r1, r2);
+    // 此位置之后 r1 和 r2 不再使用
+
+    let r3 = &mut s; // 没问题
+    println!("{}", r3);
+}
