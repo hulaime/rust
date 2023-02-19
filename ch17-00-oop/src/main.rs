d30c645686edfe43f60d6e9a52c9e8d1973d544d
metfairy <metfairy@gmail.com>
2023-02-19 13:17:31 +0000

    ch17-03-oo-design-patterns

---
+++ ch17-00-oop/src/main.rs
@@ -0,0 +1,14 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+    println!("{:?}", post);
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    println!("{}", post.content());
+    println!("{:?}", post);
+}
